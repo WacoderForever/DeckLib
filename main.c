b154c131b3a6785412ebf7c0a3d9d31302455f8f
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-05-03 19:40:17 -0300

    at

--- main.c
+++ main.c
@@ -29,11 +29,11 @@ int main(){
 
     printf("Printing sub deck:============================\n");
     Deck  *sub_deck = newDeck();
-    TransferCards(maindeck,sub_deck,5);
+    TransferCards(maindeck,sub_deck,4);
 
     for(int i = 0; i < sub_deck->size;i++){
         Card *temp= GetCardByIndex(sub_deck,i);
-        //printf("%s\n", GetCardString(temp));
+        printf("%s\n", GetCardString(temp));
     }
     printf("Printing main deck after deal:============================\n");
     for(int i=0;i<maindeck->size;i++){
